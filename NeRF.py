b3f2b1ddf77a760bdf00a62f02dc5ef0b8f0bb85
luckycontrol <beansu1@naver.com>
2022-12-30 21:40:20 +0900

    NeRF

--- NeRF.py
+++ NeRF.py
@@ -17,6 +17,16 @@ NUM_SAMPLES = 32
 POS_ENCODE_DIMS = 16
 EPOCHS = 20
 
+file_name = "tiny_nerf_data.npz"
+data = keras.utils.get_file(fname=file_name)
+data = np.load(data)
+
+images = data["images"]
+im_shape = images.shape
+(num_images, H, W, _) = images.shape
+(poses, focal) = data["poses"], data["focal"]
+
+
 def encode_position(x):
     positions = [x]
     for i in range(POS_ENCODE_DIMS)
@@ -24,7 +34,6 @@ def encode_position(x):
             positions.append(fn(2.0 ** i * x))
 
     return tf.concat(positions, axis=-1)
-
 
 def get_rays(height, width, focal, pose):
     i, j = tf.meshgrid(
@@ -64,3 +73,91 @@ def render_flat_rays(ray_origins, ray_directions, near, far, num_samples, rand=F
     rays_flat = tf.reshape(rays, [-1, 3])
     rays_flat = encode_position(rays_flat)
     return (rays_flat, t_vals)
+
+def map_fn(pose):
+    (ray_origins, ray_directions) = get_rays(height=H, width=W, focal=focal, pose=pose)
+    (rays_flat, t_vals) = render_flat_rays(
+        ray_origins=ray_origins,
+        ray_directions=ray_directions,
+        near=2.0,
+        far=6.0,
+        num_samples=NUM_SAMPLES,
+        rand=True
+    )
+
+    return (rays_flat, t_vals)
+
+split_index = int(num_images * 0.8)
+
+train_images = images[:split_index]
+val_images = images[split_index:]
+
+train_poses = poses[:split_index]
+val_poses = poses[split_index]
+
+train_img_ds = tf.data.Dataset.from_tensor_slices(train_images)
+train_pose_ds = tf.data.Dataset.from_tensor_slices(train_poses)
+train_ray_ds = train_pose_ds.map(map_fn, num_parallel_calls=AUTO)
+training_ds = tf.data.Dataset.zip((train_img_ds, train_ray_ds))
+train_ds = (
+    training_ds.shuffle(BATCH_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True, num_parallel_calls=AUTO)
+    .prefetch(AUTO)
+)
+
+val_img_ds = tf.data.Dataset.from_tensor_slices(val_images)
+val_pose_ds = tf.data.Dataset.from_tensor_slices(val_poses)
+val_ray_ds = val_pose_ds.map(map_fn, num_parallel_calls=AUTO)
+validation_ds = tf.data.Dataset.zip((val_img_ds, val_ray_ds))
+val_ds = (
+    validation_ds.shuffle(BATCH_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True, num_parallel_calls=AUTO)
+    .prefetch(AUTO)
+)
+
+def get_nerf_model(num_layers, num_pos):
+    inputs = keras.Input(shape=(num_pos, 2 * 3 * POS_ENCODE_DIMS + 3))
+    x = inputs
+    for i in range(num_layers):
+        x = layers.Dense(units=64, activation="relu")(x)
+        if i % 4 == 0 and i > 0:
+            x = layers.concatenate([x, inputs], axis=-1)
+    outputs = layers.Dense(units=4)(x)
+    return keras.Model(inputs=inputs, outputs=outputs)
+
+def render_rgb_depth(model, rays_flat, t_vals, rand=True, train=True):
+    if train:
+        predictions = model(rays_flat)
+    else:
+        predictions = model.predict(rays_flat)
+
+    predictions = tf.reshape(predictions, shape=(BATCH_SIZE, H, W, NUM_SAMPLES, 4))
+
+    rgb = tf.sigmoid(predictions[..., :-1])
+    sigma_a = tf.nn.relu(predictions[..., -1])
+
+    delta = t_vals[..., 1:] - t_vals[..., :-1]
+
+    if rand:
+        delta = tf.concat(
+            [delta, tf.broadcast_to([1e10], shape=(BATCH_SIZE, H, W, 1))], axis=-1
+        )
+        alpha = 1.0 - tf.exp(-sigma_a * delta)
+    else:
+        delta = tf.concat(
+            [delta, tf.broadcast_to([1e10], shape=(BATCH_SIZE, 1))], axis=-1
+        )
+        alpha = 1.0 - tf.exp(-sigma_a * delta[:, None, None, :])
+
+    exp_term = 1.0 - alpha
+    epsilon = 1e-10
+    transmittance = tf.math.cumprod(exp_term + epsilon, axis=-1, exclusive=True)
+    weights = alpha * transmittance
+    rgb = tf.reduce_sum(weights[..., None] * rgb, axis=-2)
+
+    if rand:
+        depth_map = tf.reduce_sum(weights * t_vals, axis=-1)
+    else:
+        depth_map = tf.reduce_map(weights * t_vals[:, None, None], axis=-1)
+
+    return (rgb, depth_map)
