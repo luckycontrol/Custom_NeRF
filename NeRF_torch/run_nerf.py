2c69272b7d9b84d480e4b50da6ecdfbc58e367c3
luckycontrol <beansu1@naver.com>
2023-01-02 02:09:49 +0900

    NeRF_torch

--- NeRF_torch/run_nerf.py
+++ NeRF_torch/run_nerf.py
@@ -1,55 +1,28 @@
+import os
+import glob
+import imageio
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 import torch
+import torchvision
 
-from load_data import MyDataset, file_name
+BATCH_SIZE = 5
+NUM_SAMPLES = 32
+POS_ENCODE_DIMS = 16
+EPOCHS = 20
 
-dataset = MyDataset(file_name)
+file_name = "tiny_nerf_data.npz"
+url = "https://people.eecs.berkeley.edu/~bmild/nerf/tiny_nerf_data.npz"
+if not os.path.exists(file_name):
+    data = data = torch.hub.download_url_to_file(url, file_name)
 
+data = np.load(file_name)
+images = data["images"]
+im_shape = images.shape
+(num_images, H, W, _) = images.shape
+poses = data["poses"]
+focal = data["focal"]
 
-def encode_position(x):
-    dim = 10
-    positions = []
-
-    for i in range(dim):
-        for fn in [torch.sin, torch.cos]:
-            positions.append(fn(2.0 ** i * x))
-
-    return torch.concat(positions, dim=1)
-
-
-def get_rays(height, width, focal, pose):
-    i, j = torch.meshgrid(
-        torch.range(width, dtype=torch.float32),
-        torch.range(height, dtype=torch.float32),
-        indexing="xy"
-    )
-
-    transformed_i = (i - width * 0.5) / focal
-    transformed_j = (j - height * 0.5) / focal
-
-    directions = torch.stack([transformed_i, -transformed_j, torch.ones_like(i)], dim=2)
-
-    camera_matrix = pose[:3, :3]
-    height_width_focal = pose[:3, -1]
-
-    transformed_dirs = directions[..., None, :]
-    camera_dirs = transformed_dirs * camera_matrix
-    ray_directions = torch.sum(camera_dirs, dim=-1)
-    ray_origins = torch.broadcast_to(height_width_focal, ray_directions.shape)
-
-    return ray_origins, ray_directions
-
-
-def render_flat_rays(ray_origins, ray_directions, near, far, num_samples, rand=False):
-    t_vals = torch.linspace(near, far, num_samples)
-
-    if rand:
-        shape = list(ray_origins.shape[:-1]) + [num_samples]
-        noise = torch.rand(size=shape) * (far - near) / num_samples
-        t_vals = t_vals + noise
-
-    rays = ray_origins[..., None, :] + (
-            ray_directions[..., None, :] * t_vals[..., None]
-    )
-    rays_flat = torch.reshape(rays, [-1, 3])
-    rays_flat = encode_position(rays_flat)
-    return rays_flat, t_vals
+print(focal)
